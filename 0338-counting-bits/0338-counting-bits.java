c482527853d740cf13b2e19f86fb1a9a4af9b581
Shubham Satnalika <99472229+shubhsat@users.noreply.github.com>
2022-11-16 21:48:21 +0530

    Time: 21 ms (12.89%), Space: 52.7 MB (6.36%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans =new int[n+1];
+        for(int i= 0; i<=n; i++){
+            String bin = Integer.toBinaryString(i);
+            int count = 0;
+            for (int j = 0; j < bin.length(); j++) {
+                if (bin.charAt(j) == '1') {
+                    count++;
+                }
+            }
+            ans[i] = count;
+        }
+        return ans;
+    }
+}
